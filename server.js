d1b41f471bbdc0cbec3a182bcd5280af8fad868c
CJ O'Sullivan <me@cjosullivan.net>
2018-01-16 11:40:55 +0000

    Static index page added.

--- server.js
+++ server.js
@@ -2,13 +2,17 @@ var morgan      = require('morgan');
 var express     = require('express');
 var mongoose    = require('mongoose');
 var bodyParser  = require('body-parser');
-var User        = require('./app/models/user');
+var router      = express.Router();
+var appRoutes   = require('./app/routes/api')(router);
 var app         = express();
 var port        = process.env.PORT || 8080;
+var path        = require('path');
 
 app.use(morgan('dev'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true}));
+app.use(express.static(__dirname + '/public'));
+app.use('/api', appRoutes);
 
 mongoose.connect('mongodb://localhost:27017/fyp_shyft', function(err) {
     if(err) {
@@ -18,26 +22,8 @@ mongoose.connect('mongodb://localhost:27017/fyp_shyft', function(err) {
     }
 });
 
-app.post('/users', function(req, res) {
-    var user = new User();
-
-    user.username = req.body.username;
-    user.password = req.body.password;
-    user.email = req.body.email;
-
-    if (req.body.username == null || req.body.username == '' || req.body.password == null || req.body.password == '' || req.body.email == null || req.body.email == '') {
-
-        res.send('Ensure username, email and password were provided');
-
-    } else {
-        user.save(function(err) {
-            if(err){
-                res.send('Username or Email already exists.');
-            } else {
-                res.send('User Created.');
-            }
-        });
-    }
+app.get('*', function(req, res) {
+   res.sendFile(path.join(__dirname + '/public/app/views/index.html'));
 });
 
 //Process.env.PORT - If deploying to an environment that requires a certain port then it will use that one.
